cbf81d61c32eb1a9032021e939217c6453e1de3c
JayJamesJr <jjjames@miners.utep.edu>
2020-02-26 18:56:36 -0700

    msg

--- file_exists.c
+++ file_exists.c
@@ -1,12 +1,8 @@
 #include"file_exists.h"
 #define NUM_PATHS 6
 int file_exists(char* filepath){
-	char * path = getenv("PATH");
-	char ** PATH = extract_tokens(path,":");
-	for(int i = 0; i < NUM_PATHS; i++){
-		if (access(filepath, F_OK) != -1){
-			return 1;	
-		}	
-	}
+	if (access(filepath, F_OK) != -1){
+		return 1;	
+	}		
 	return 0;
 }
